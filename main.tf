c7b9815ca203ea08c35cc414db9f18b9fab502f5
EliFiat <eli.fiatuwo@gmail.com>
2022-10-28 00:25:17 -0400

    2

--- main.tf
+++ main.tf
@@ -39,3 +39,41 @@ module "vpc" {
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+
+### EC2 Instance
+resource "aws_instance" "web" {
+  ami           = var.ami_ids
+  # availability_zone = slice(data.aws_availability_zones.available.names, 0,3)
+  availability_zone = var.azs[count.index]
+  instance_type = var.instance_type
+  count = length(var.ec2_name_tag)
+  # subnet_id = var.subnet_ids[count.index]
+  subnet_id = module.vpc.private_subnets[count.index]
+
+  tags = {
+    Name = var.ec2_name_tag[count.index]
+  }
+}
+
+### EC2 LB target group
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+# ### TARGET GROUP ATTACHMENT
+# resource "aws_lb_target_group_attachment" "web-tg-attach" {
+#   target_group_arn = aws_lb_target_group.web_target_group.arn
+#   target_id        = aws_instance.web.id[*]
+#   port             = 80
+# }
+
+
+
+# data "aws_subnets" "private" {
+#   vpc_id = module.vpc.vpc_id
+
+# }
--- variables.tf
+++ variables.tf
@@ -13,3 +13,30 @@ variable "public_cidr" {
     type = list 
     default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
+
+# Below is the EC2 variables blocks
+variable "ec2_name_tag" {
+  default = ["web1", "web2","web3"]
+}
+
+variable "instance_type" {
+    type = string
+  default = "t2.micro"
+}
+
+variable "ami_ids" {
+  default = "ami-09d3b3274b6c5d4aa"
+}
+
+variable "azs" { 
+    default = {
+        0 = "us-east-1a"
+        1 = "us-east-1b"
+        2 = "us-east-1c"
+    }
+}
+
+# variable "vpc_id" {
+#     default = module.vpc.vpc_id
+#  }
+
